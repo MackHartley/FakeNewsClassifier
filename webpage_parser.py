4836fb7b878226409712605d9b29474a0078e76b
tianyoul <youzi323@live.cn>
2018-04-22 16:27:08 -0500

    Get title, content and outlinks given an url

---
+++ webpage_parser.py
@@ -0,0 +1,69 @@
+from bs4 import BeautifulSoup
+import re
+import urllib
+import os
+import requests
+
+def process_page(url):  #return the title (string), content (list) and all the out links (list)
+    try:
+        page = urllib.request.urlopen(url)
+    except urllib.error.HTTPError:
+        print('URL not valid.')
+        return '', [], []
+    soup = BeautifulSoup(page, "lxml")
+    titles = soup.find_all('title')
+
+    title = ''
+    if len(titles) != 0:
+        title_str = str(titles[0])
+        title = clean_string(title_str)
+
+    content = []
+    paragraphs = soup.find_all("p")
+    for p in paragraphs:
+        para = str(p)
+        content.append(clean_string(para))
+
+    outlinks = []
+    prefix = find_prefix(url)
+    for link in soup.findAll('a', href=True):
+        url = link.get('href')
+        if url[0] == '/':
+            url = prefix + url
+        elif url[0:4] != 'http':
+            continue
+        outlinks.append(url)
+
+
+    return title, content, outlinks
+
+
+def get_title(url):   # Get the first title from a page
+    try:
+        page = urllib.request.urlopen(url)
+    except urllib.error.HTTPError:
+        print('HTTPError')
+        return ''
+    soup = BeautifulSoup(page, "lxml")
+    titles = soup.find_all('title')
+    if len(titles) != 0:
+        title_str = str(titles[0])
+        clean_str = clean_string(title_str)
+        return clean_str
+    else:
+        return ''
+
+
+
+def clean_string(line):   # <p>Hello</p> -> Hello
+    result = re.sub(r'<[^<>]*>', '', line)
+    return result
+
+
+def find_prefix(url):   # E.g : Find http://www.bbc.com from http://www.bbc.com/news/blogs-trending-43745629
+    ind = url.index('.com')
+    if ind == -1:
+        print('No prefix found')
+        return ''
+    else:
+        return url[0 : ind + 4]
